9eed3e90606fea5590fa9b74f05a6012ef81c9d2
Matheus Verissimo <mverissimo@10.0.0.105>
2020-02-03 22:46:33 -0300

    feat(container): add repository issues page

--- src/pages/repository/Repository.js
+++ src/pages/repository/Repository.js
@@ -1,47 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { useQuery } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
+import { QUERY_ISSUES } from 'graphql/repositories';
 
-const QUERY = gql`
-  query issues($slug: String!) {
-    repository(owner: $slug, name: "vagas") {
-      issues(last: 20, states: OPEN) {
-        edges {
-          node {
-            title
-            url
-          }
-        }
-      }
-    }
-  }
-`;
+import { Container, Row, Col } from 'react-awesome-styled-grid';
+import { ListIssues, ListIssuesLoading } from 'components/list-issues';
 
+import { useInfiniteScroll } from 'react-infinite-scroll-hook';
 function RepositoryPage({ match }) {
-  const { loading, error, data } = useQuery(QUERY, {
-    variables: { slug: match.params.slug }
+  const [isFetching, setIsFetching] = useState(true);
+  const [hasNextPage, setHasNextPage] = useState(true);
+
+  const { loading, error, data, fetchMore } = useQuery(QUERY_ISSUES, {
+    variables: {
+      slug: match.params.slug
+    },
+    notifyOnNetworkStatusChange: false
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  const handleLoadMore = (fetchMore, issues) => {
+    return fetchMore({
+      variables: {
+        after: issues.pageInfo.endCursor
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev;
 
-  console.log(data.repository);
+        const newEdges = fetchMoreResult.repository.issues.edges;
+        const pageInfo = fetchMoreResult.repository.issues.pageInfo;
+
+        setHasNextPage(pageInfo.hasNextPage);
+
+        return {
+          repository: {
+            ...prev,
+            __typename: fetchMoreResult.repository.__typename,
+            issues: {
+              edges: [...prev.repository.issues.edges, ...newEdges],
+              __typename: fetchMoreResult.repository.issues.__typename,
+              pageInfo
+            }
+          }
+        };
+      }
+    });
+  };
+
+  const ListRef = useInfiniteScroll({
+    loading,
+    hasNextPage,
+    onLoadMore: () => handleLoadMore(fetchMore, data.repository.issues)
+  });
+
+  if (error) return <p>error</p>;
 
   return (
-    <div>
-      {data.repository.issues.edges.map(({ node: { title } }, index) => (
-        <p key={`${title}-${index}`}>{title}</p>
-      ))}
-    </div>
+    <Container>
+      <Row>
+        <Col xs={12} lg={8}>
+          {loading ? (
+            <ListIssuesLoading />
+          ) : (
+            <div ref={ListRef}>
+              <ListIssues data={data.repository.issues.edges} />
+            </div>
+          )}
+        </Col>
+
+        <Col xs={12} lg={4}>
+          <div>
+            <button
+              onClick={() => handleLoadMore(fetchMore, data.repository.issues)}
+            >
+              get more
+            </button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
 RepositoryPage.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.object
-  })
+  }).isRequired
 };
 
 export default RepositoryPage;
