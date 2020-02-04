3e2d214066782d66c234c7fa7c7282ef51c10f62
Matheus Verissimo <mverissimo@10.0.0.105>
2020-02-03 22:46:15 -0300

    feat(container): add list repositories page

--- src/pages/repositories/Repositories.js
+++ src/pages/repositories/Repositories.js
@@ -1,34 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+import { Container } from 'react-awesome-styled-grid';
+import { ListRepositories } from 'components/list-repositories';
 
 const REPOSITORIES = [
   {
     name: 'Front-end Brasil',
-    description: 'O mundo frontender dentro do Github beer',
-    slug: 'frontendbr'
+    description: 'O mundo frontender dentro do Github beer.',
+    slug: 'frontendbr',
+    image: '16963863'
   },
   {
     name: 'Android Dev BR',
-    description:
-      'Comunidade de desenvolvedores Android brasileiros. Junte-se a nós! :)',
-    slug: 'androiddevbr'
+    description: 'Comunidade de desenvolvedores Android brasileiros.',
+    slug: 'androiddevbr',
+    image: '13825651'
   },
   {
     name: 'CocoaHeads Brasil',
-    description: 'Repositório Público do CocoaHeads Brasil',
-    slug: 'CocoaHeadsBrasil'
+    description: 'Repositório Público do CocoaHeads Brasil.',
+    slug: 'CocoaHeadsBrasil',
+    image: '7914270'
   }
 ];
 
 function RepositoriesPage() {
   return (
-    <div>
-      {REPOSITORIES.map(({ name, description, slug }, index) => (
-        <Link to={slug} key={name}>
-          {name}
-        </Link>
-      ))}
-    </div>
+    <Container>
+      <ListRepositories data={REPOSITORIES} />
+    </Container>
   );
 }
 
